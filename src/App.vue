07cbd97e113af7d4494c0edbdb06ae415409fe0d
yannchuu <813478178@qq.com>
2019-09-23 20:34:27 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>
+        <div class="header-left">xxx</div>
+        <div class="header-center">
+          <el-menu
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">我的工作台</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+              <el-menu-item index="2-3">选项3</el-menu-item>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3">消息中心</el-menu-item>
+          </el-menu>
+        </div>
+        <div class="header-right">xxx</div>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    handleSelect() {}
   }
-}
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+html,
+body {
+  height: 100%;
+  color: #000;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+}
+
+.el-header {
+  background-color: #545c64;
+  z-index: 99;
+  padding: 0 !important;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header-left {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+.el-footer {
+  /* background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 60px; */
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
 }
 </style>
---
+++ src/common/api/http.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import qs from 'qs';
+import { GET, POST, PUT, DELETE } from './methods';
+
+const instance = axios.create({
+  baseURL: '',
+  headers: {},
+  withCredentials: true
+});
+
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+
+const fetch = (options) => {
+  const { method = POST, data, url, config } = options;
+  switch (method.toLowerCase()) {
+    case GET:
+      return instance.get(`${url}${options.data ? `?${qs.stringify(options.data)}` : ''}`, config);
+    case POST:
+      return instance.post(url, data, config);
+    case PUT:
+      return instance.put(url, qs.stringify(data), config);
+    case DELETE:
+      return instance.delete(`${url}${options.data ? `?${qs.stringify(options.data)}` : ''}`, config);
+    default:
+      return instance(options);
+  }
+};
+// http异步请求
+export default async function http(options) {
+  return fetch(options)
+    .then((response) => {
+      return response;
+    }).catch((error) => {
+      return error;
+    });
+}
